e614484e4be5aa7cffd8bf11c4524999db3d0b84
ChinthakaICIEOS <chinthaka@icieos.com>
2025-06-28 16:59:35 +0530

    Page Does not Found ???

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
-
 const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
-
 const shopRoutes = require("./routes/shop");
 
 const app = express();
@@ -11,7 +9,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended : false}));
 
 app.use(adminRoutes);
-
 app.use(shopRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).send('<h2>Page Does Not Found</h2>');
+})
 
 app.listen(3000);
