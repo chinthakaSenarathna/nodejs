54044e3a426f881a9e1c0fc4b9250cd440cbbe10
ChinthakaICIEOS <chinthaka@icieos.com>
2025-06-28 15:48:39 +0530

    Routes in the Express ???

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const express = require("express");
 const app = express();
 
 // add middleware
-app.use((req,res,next) => {
-    console.log("In the middleware");
+app.use('/',(req,res,next) => {
+    console.log("always run");
     next(); // allows request to the continue to the next middleware....
 })
 
-app.use((req,res,next) => {
-    console.log("another middleware");
-    res.send('<h2>From the ExpressJs</h2>');
+app.use('/add-product',(req,res,next) => {
+    console.log("add products middleware");
+    res.send('<h2>Add the product page</h2>');
+})
+
+app.use('/',(req,res,next) => {
+    console.log("default middleware");
+    res.send("<h2>From the Express</h2>");
 })
 
 app.listen(3000);
